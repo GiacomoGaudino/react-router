49415c686053453d2c621def90f321cd9fe6286d
GiacomoGaudino <giacomo.gaudino@yahoo.it>
2025-10-18 11:53:35 +0200

    Adjusted CSS for the jumbotron

--- src/components/AppJumbotron.jsx
+++ src/components/AppJumbotron.jsx
@@ -1,13 +1,19 @@
+import intro from '../assets/imgs/intro.svg'
+
 export default function AppJumbotron() {
 
     return (
         <div className="p-5 mb-4 rounded-3" id="jumbotron">
-            <div className="container-fluid py-5">
-                <h1 className="display-5 fw-bold">My e-Commerce</h1>
-                <p className="col-md-8 fs-4">
-                    Discover curated products, exclusive deals, and a fast, secure checkout — your
-                    one‑stop shop for quality items and great prices.
-                </p>
+            <div className="container d-flex gap-3 py-5">
+                <div className='col-md-5'>
+
+                    <h1 className="display-5 fw-bold">My e-Commerce</h1>
+                    <p className="fs-4">
+                        Discover curated products, exclusive deals, and a fast, secure checkout — your
+                        one‑stop shop for quality items and great prices.
+                    </p>
+                </div>
+                <img className='w-50' src={intro} alt="" />
             </div>
         </div>
 
